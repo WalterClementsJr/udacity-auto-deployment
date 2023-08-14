f94f9d9f7d0fbaf86556cdbde8dc73456de618a7
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-08-14 22:25:52 +0700

    fix fe + be dependencies

--- backend/src/modules/domain/employees/entities/employee.entity.ts
+++ backend/src/modules/domain/employees/entities/employee.entity.ts
@@ -59,10 +59,10 @@ export class Employee extends AggregateRoot<number> {
   public personalEmail: string;
 
   @Column({ nullable: true })
-  public birthdate: Date;
+  public birthdate: string;
 
   @Column()
-  public startDate: Date;
+  public startDate: string;
 
   @Column({ length: 200, nullable: true })
   public address: string;
@@ -92,7 +92,7 @@ export class Employee extends AggregateRoot<number> {
   public city: string;
 
   @Column()
-  public effectiveDate: Date;
+  public effectiveDate: string;
 
   @Column('decimal')
   public salary: number;
@@ -117,7 +117,7 @@ export enum SalaryType {
   HOURLY = 'hourly',
 }
 
-export function getDateFromString(date: string): Date {
+export function getDateFromString(date: string): string {
   return moment(date).format('M/D/YYYY');
 }
 
